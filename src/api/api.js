491f8c3a2d0b0122471991d01c15841738639b69
Siano-Tech <122805834+Siano-Tech@users.noreply.github.com>
2024-08-17 13:05:04 +0530

    Add files via upload

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,8 @@
 import axios from 'axios';
 
 // Default Axis Endpoint for all the requests
-axios.defaults.baseURL = 'http://192.168.0.101:5000';
-// axios.defaults.baseURL = 'https://book-keeper-backend.vercel.app';
+if(window.location.hostname.includes('localhost')) {
+    axios.defaults.baseURL = 'http://192.168.0.101:5000';
+} else {
+    axios.defaults.baseURL = 'https://website-builder-server.vercel.app';
+}
