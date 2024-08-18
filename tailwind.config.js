be3097984880dc0efad4e272c22c520bd0831ae7
Siano-Tech <122805834+Siano-Tech@users.noreply.github.com>
2024-08-18 19:59:23 +0530

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,23 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'indigo': {
+          '50': '#f4f2ff',
+          '100': '#ece7ff',
+          '200': '#dad3ff',
+          '300': '#c0b0ff',
+          '400': '#a183ff',
+          '500': '#8450ff',
+          '600': '#813efb',
+          '700': '#671be6',
+          '800': '#5616c1',
+          '900': '#48149e',
+          '950': '#2b0a6b',
+        },
+      },
+    },
   },
   plugins: [
     require('@headlessui/tailwindcss')({ prefix: 'ui' })
